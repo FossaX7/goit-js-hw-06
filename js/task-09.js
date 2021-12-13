7eecb6a0faf5dcd055a5dcc7338ea58517138f20
Andrii Mogylnytskyi <88145575+FossaX7@users.noreply.github.com>
2021-12-13 22:19:01 +0200

    Task 7-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const changeColorBtn = document.querySelector('.change-color');
+const currentColor = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', () => {
+  const colorBody = getRandomHexColor();
+  document.body.style.backgroundColor = colorBody;
+  currentColor.textContent = colorBody;
+});
